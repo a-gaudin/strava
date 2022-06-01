46fca1f4af77aefec03322ccddcb298826ee0ed5
Alexis Gaudin <alexis.gaudin@gadz.org>
2022-06-01 16:53:45 +0200

    added new columns to activities_df

--- main.py
+++ main.py
@@ -10,20 +10,30 @@ def get_activities(access_token, params):
     header = {'Authorization': 'Bearer ' + access_token}
     activities_json = requests.get(activites_url, headers=header, params=params).json()
     activities_df = pd.json_normalize(activities_json)
-    activities_df = activities_df[['name', 'distance', 'moving_time', 'elapsed_time', 'total_elevation_gain', 'type', 'start_date', 'average_speed', 'max_speed']]
-    return activities_df
+    return activities_df[['name', 'distance', 'moving_time', 'elapsed_time', 'total_elevation_gain', 'type', 'start_date', 'average_speed']]
 
-def change_units(activities_df):
-    activities_df["distance"] = activities_df["distance"] / 1000
-    activities_df["elapsed_time"] = pd.to_datetime(activities_df["elapsed_time"], unit='s')
-    activities_df["average_speed"] = activities_df["average_speed"] * 3.6
-    return activities_df
+def change_units(df):
+    df.rename(columns = {'average_speed':'moving_speed', 'total_elevation_gain':'elevation_gain'}, inplace = True)
+    df["distance"] = df["distance"] / 1000
+    df["moving_speed"] = df["moving_speed"] * 3.6
+    return df
+
+def add_new_columns(df):
+    df["elapsed_time_hhmmss"] = pd.to_datetime(df["elapsed_time"], unit='s').dt.time
+    df["speed"] = df["distance"] / (df["elapsed_time"] / 3600)
+    df["grade_adjusted_speed"] = (df["distance"] + (df["elevation_gain"] / 1000)) / (df["elapsed_time"] / 3600)
+    df["average_slope"] = (df["elevation_gain"] / (df["distance"] / 2 * 1000)) * 100
+    return df
 
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     access_token = get_access_token(config.token_request_params)
     activities_df = get_activities(access_token, config.activities_request_params)
     activities_df = change_units(activities_df)
+    activities_df = add_new_columns(activities_df)
+
+    pd.set_option('display.max_columns', None)
+    print(activities_df.head())
     
 if __name__ == "__main__":
     import config

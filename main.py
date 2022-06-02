da25720b312f9a42cb2d0b9ce085adaed028cca0
Alexis Gaudin <alexis.gaudin@gadz.org>
2022-06-02 10:53:51 +0200

    pivot table only on run and ride activities

--- main.py
+++ main.py
@@ -53,10 +53,11 @@ def add_new_columns(df):
 
 def get_summary(df):
     """ Returns a pivot table dataframe that summarizes the activities """
+    df = df[df["type"].isin(["Run", "Ride"])]
     pivot_table = pd.pivot_table(df, values=['distance', 'elapsed_time', 'elevation_gain'],
                                 index=['month'], columns=['type'], aggfunc=np.sum, fill_value=0)
-    #pivot_table = pivot_table.sort_values(by="month", ascending=False).index
-    return pivot_table #[["Run", "Bike"]]
+    pivot_table = pivot_table.sort_values(by=["month"], ascending=False)
+    return pivot_table
 
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -71,7 +72,7 @@ def main():
     print(activities_df.head())
 
     summary_df = get_summary(activities_df)
-    print(summary_df.head())
+    print(summary_df.head(10))
     
 if __name__ == "__main__":
     main()

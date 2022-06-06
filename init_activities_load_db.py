e7f19596a1a0714ad2d88e492bee1a477b74d28f
Alexis Gaudin <alexis.gaudin@gadz.org>
2022-06-06 18:31:02 +0200

    finalized get_load and add_load

--- init_activities_load_db.py
+++ init_activities_load_db.py
@@ -2,25 +2,27 @@ import pandas as pd
 import strava_api
 
 def get_load(id):
-    """ Returns a dictionary with rate of perceived exertion and description
+    """ Returns a dictionary with rates of perceived exertion and notes
         (that may contain soreness/pain information) """
-    activity_json = strava_api.get_activity(id)
-    perceived_exertion = activity_json['perceived_exertion']
-    description = activity_json['description']
-
-    return {perceived_exertion: perceived_exertion, description: description}
+    activity_json = strava_api.get_activity(id) 
+    perceived_exertion = activity_json['perceived_exertion'] if 'perceived_exertion' in activity_json else float("NaN")
+    note = activity_json['private_note'] if 'private_note' in activity_json else ''
+    return {'perceived_exertion': perceived_exertion, 'note': note}
 
 def add_load(activities_df):
     """ Adds the training load information to the activities dataframe """
     load_list = [get_load(id) for id in activities_df['id']]
-    print(load_list)
+    activities_df['perceived_exertion'] = [x['perceived_exertion'] for x in load_list]
+    activities_df['note'] = [x['note'] for x in load_list]
+    return activities_df
 
 def main():
     activities_df = pd.read_pickle("./db/activities.pkl")
-    activities_load_df = add_load(activities_df)
+    activities_df = add_load(activities_df)
+    # activities_df = note_to_soreness(activities_df)
 
     pd.set_option('display.max_columns', None)
-    # print(activities_load_df.head(10))
+    print(activities_df.head(10))
 
 if __name__ == "__main__":
     main()

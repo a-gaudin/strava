27e5e0042fe833bb330161d7821960fcb02d7fb9
Alexis Gaudin <alexis.gaudin@gadz.org>
2022-06-17 23:20:22 +0200

    corrected path to extract db

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from data_extract.extract import Extract
 from data_transform.transform import Transform
 
 def main() -> None:
-    Extract().update_strava_activities_db()
+    # Extract().update_strava_activities_db()
 
     cfg = get_config()
-    strava_activities_db_filename = Path(cfg.db.extract.folder_path) / cfg.db.activities_file_name
+    strava_activities_db_filename = Path(cfg.db.extract.folder_path) / cfg.db.extract.activities_file_name
     strava_activities_df = pd.read_pickle(strava_activities_db_filename)  
     Transform().create_activities_db(strava_activities_df)
-    
+
     print('hello')
 
 if __name__ == "__main__":
